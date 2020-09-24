194d6e888e47a949e3ce958628334aaa9ce65b80
Sean <ubuntu@ip-172-31-44-214.ec2.internal>
2020-09-24 23:00:21 +0000

    need to clone onto new ec2

--- main.py
+++ main.py
@@ -4,14 +4,12 @@ from Flights import Flight
 from Itinerary import Itinerary
 
 
-
 origin = input("Where are you flying from?\n")
 destination= input("Where would you like to go\n")
 strarting_range= input("Looking at flights from the date?\n")
 ending_range= input("To the date\n")
 
 response = requests.get(f"https://api.skypicker.com/flights?flyFrom={origin}&to={destination}&curr=AUD&dateFrom={strarting_range}&dateTo={ending_range}&partner=picky&v=3")
-
 data = json.loads(response.text)
 
 for flights in data["data"]:
@@ -25,5 +23,6 @@ Itinerary.choice(int(user_input))
 
 
 
-
 # response = requests.get(f"https://api.skypicker.com/flights?flyFrom=MEL&to=SYD&curr=AUD&dateFrom=05/10/2020&dateTo=10/10/2020&partner=picky&v=3")
+# data = json.loads(response.text)
+# print(data["data"])
