b8d0efead1d45ad64f829ff81afe63d9baf1be12
Sean <ubuntu@ip-172-31-44-214.ec2.internal>
2020-09-24 05:07:44 +0000

    clean main.py

--- Itinerary.py
+++ Itinerary.py
@@ -1,8 +1,41 @@
+import os
+
 class Itinerary():
     itineraries = {}
-    count = 0
+    count = 1
     
     @classmethod
     def add_flight(cls, flight):
         cls.itineraries[cls.count]=flight
         cls.count +=1
+    
+    @classmethod
+    def price_sort(cls):
+        for items in cls.itineraries:
+            print(f"""{items}   Cost: {cls.itineraries[items].price}
+    From: {cls.itineraries[items].origin}    at    {cls.itineraries[items].departure_time}
+    To : {cls.itineraries[items].destination}    at    {cls.itineraries[items].arrival_time}
+    Extra info:
+        Route: {cls.itineraries[items].routes}
+        Airline Carrier : {cls.itineraries[items].airlines}
+        Duration: {cls.itineraries[items].duration}
+        Transfers : {cls.itineraries[items].transfer}
+*******************************************************************************""")
+
+    @classmethod
+    def choice(cls, int):
+        os.system('clear')
+        print(f"""*******************************************************************************
+{int}   Cost: {cls.itineraries[int].price}
+    From: {cls.itineraries[int].origin}    at    {cls.itineraries[int].departure_time}
+    To : {cls.itineraries[int].destination}    at    {cls.itineraries[int].arrival_time}
+    Extra info:
+        Route: {cls.itineraries[int].routes}
+        Airline Carrier : {cls.itineraries[int].airlines}
+        Duration: {cls.itineraries[int].duration}
+        Transfers : {cls.itineraries[int].transfer}
+        
+Link to Booking: {cls.itineraries[int].deep_link}        
+*******************************************************************************""")
+        
+        
--- main.py
+++ main.py
@@ -5,28 +5,25 @@ from Itinerary import Itinerary
 
 
 
-# origin = input("Where are you flying from?\n")
-# destination= input("Where would you like to go\n")
-# strarting_range= input("Looking at flights from the date?\n")
-# ending_range= input("To the date\n")
+origin = input("Where are you flying from?\n")
+destination= input("Where would you like to go\n")
+strarting_range= input("Looking at flights from the date?\n")
+ending_range= input("To the date\n")
 
-# response = requests.get(f"https://api.skypicker.com/flights?flyFrom={origin}&to={destination}&curr=AUD&dateFrom={strarting_range}&dateTo={ending_range}&partner=picky&v=3")
-
-response = requests.get(f"https://api.skypicker.com/flights?flyFrom=MEL&to=SYD&curr=AUD&dateFrom=05/10/2020&dateTo=10/10/2020&partner=picky&v=3")
+response = requests.get(f"https://api.skypicker.com/flights?flyFrom={origin}&to={destination}&curr=AUD&dateFrom={strarting_range}&dateTo={ending_range}&partner=picky&v=3")
 
 data = json.loads(response.text)
-
-# g=[enumerate(Flight(flights)) for flights in data["data"]]
-# print(g)
-    
 
 for flights in data["data"]:
     Itinerary.add_flight(Flight(flights))
 
+Itinerary.price_sort()
 
-print(Itinerary.itineraries)
+user_input = input("\n\nWhich Flight would you like? enter reference num\n\n")
 
-# for items in data["data"]:
-#     print(f'From {items["cityFrom"]}, {items["countryFrom"]["name"]} to {items["cityTo"]}, {items["countryTo"]["name"]} for {items["price"]}')
+Itinerary.choice(int(user_input))
 
 
+
+
+# response = requests.get(f"https://api.skypicker.com/flights?flyFrom=MEL&to=SYD&curr=AUD&dateFrom=05/10/2020&dateTo=10/10/2020&partner=picky&v=3")
