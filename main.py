fbad1105299088c25c7d50ad7bf4e3a08f16a4fb
Sean <ubuntu@ip-172-31-44-214.ec2.internal>
2020-09-24 02:33:06 +0000

    broke down my Flights class

--- main.py
+++ main.py
@@ -13,7 +13,20 @@ class Flights():
     def get_destination(self):
         self.destination = f'{self.flight_data["cityTo"]}, {self.flight_data["countryTo"]["name"]}' 
 
-    
+    def get_price(self):
+        self.price = self.flight_data["price"]
+        
+    @staticmethod
+    def convert_timestamp(timestamp):
+        timestamp = datetime.datetime.fromtimestamp(timestamp)
+        conv_timestamp=timestamp.astimezone(pytz.timezone("Australia/Melbourne"))
+        return conv_timestamp.strftime('%d/%m/%y %H:%M:%S')
+        
+    def get_departure_time(self):
+        self.departure_time = self.convert_timestamp(self.flight_data['dTimeUTC'])
+
+    def get_arrival_time(self):
+        self.arrival_time = self.convert_timestamp(self.flight_data['aTimeUTC'])    
 # origin = input("Where are you flying from?\n")
 # destination= input("Where would you like to go\n")
 # strarting_range= input("Looking at flights from the date?\n")
@@ -26,23 +39,10 @@ response = requests.get(f"https://api.skypicker.com/flights?flyFrom=MEL&to=SYD&c
 data = json.loads(response.text)
 
 flight1=Flights(data["data"][0])
-flight1.get_origin()
-print(flight1.origin)
+flight1.get_departure_time()
+print(flight1.departure_time)
 
 # for items in data["data"]:
 #     print(f'From {items["cityFrom"]}, {items["countryFrom"]["name"]} to {items["cityTo"]}, {items["countryTo"]["name"]} for {items["price"]}')
 
-# def get_sunset(num):
-#     timestamp = datetime.datetime.fromtimestamp(num)
-#     conv_timestamp=timestamp.astimezone(pytz.timezone("Australia/Melbourne"))
-#     return conv_timestamp.strftime('%d/%m/%y %H:%M:%S')
 
-
-# print(get_sunset(data['data'][0]['dTimeUTC']))
-
-# print(data['data'][0]["fly_duration"])
-# print(data['data'][0]["cityFrom"])
-# print(data['data'][0]["countryFrom"]["name"])
-# print(data['data'][0]["cityTo"])
-# print(data['data'][0]["countryTo"]["name"])
-# print(data['data'][0]["price"])
