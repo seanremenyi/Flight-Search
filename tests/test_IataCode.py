bfcdb56fe195f73db6436eb800b60a5dadc20c6e
Sean <ubuntu@ip-172-31-44-214.ec2.internal>
2020-10-01 22:37:07 +0000

    organized file tree

--- tests/test_IataCode.py
+++ tests/test_IataCode.py
@@ -1,18 +1,21 @@
-# import unittest
-# from IataCode import IataCode
-# from mock import patch
+from mock import patch
+import unittest
+import sys
+sys.path.append('../src')
+from IataCode import IataCode
 
-# class TestIataCodeClass(unittest.TestCase):
-#     def test_IataCode_methods(self):
-#         with patch.object(IataCode, "__init__", lambda x, y: None):
-#             test_object=IataCode("which city?")
+
+class TestIataCodeClass(unittest.TestCase):
+    def test_IataCode_methods(self):
+        with patch.object(IataCode, "__init__", lambda x, y: None):
+            test_object=IataCode("which city?")
         
-#             test_values = test_object.load_iata_codes()
+            test_values = test_object.load_iata_codes()
         
-#             self.assertEqual(type(test_values), dict)
-#             self.assertEqual(test_values["Aarhus"][1], "AAR")
-#             self.assertEqual(test_values["Bintulu"][1], "BTU")
-#             self.assertEqual(test_values['Whale Cove, NT'][1], "YXN")
+            self.assertEqual(type(test_values), dict)
+            self.assertEqual(test_values["Aarhus"][1], "AAR")
+            self.assertEqual(test_values["Bintulu"][1], "BTU")
+            self.assertEqual(test_values['Whale Cove, NT'][1], "YXN")
 
 
   
