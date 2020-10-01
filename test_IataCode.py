ca0501d9376b8459d6a4a383626d014a896ed138
Sean <ubuntu@ip-172-31-44-214.ec2.internal>
2020-10-01 02:01:01 +0000

    testing

---
+++ test_IataCode.py
@@ -0,0 +1,29 @@
+import unittest
+from unittest.mock import patch
+from IataCode import IataCode
+
+class TestIataCodeClass(unittest.TestCase):
+    @patch('IataCode.self.city_choice', return_value='python')
+    def test_city_choice(self, input):
+        """Test city_choice()"""
+        self.assertTrue("Python")
+        
+
+
+
+
+        # @patch('IataCode.self.get_code', return_value='python')
+        # def test_get_iata(self, input):
+        #     self.assertEqual(answer(), 'you entered yes')
+    #         question.title
+    #         question.options
+    #         question.answer
+    #     except Exception as error:
+    #         self.assertTrue(False, error)
+
+    # def test_is_correct(self):
+    #     """Test quiz.is_correct()"""
+    #     question = Question("a", [1,2,3], 3)
+    #     self.assertTrue(question.is_correct(3))
+    #     self.assertFalse(question.is_correct(2))
+    #     self.assertFalse(question.is_correct(1))
---
+++ tests/test_IataCode.py
@@ -0,0 +1,7 @@
+# import sys
+# sys.path.append("..")
+# from ../IataCode import IataCode
+import .. flight.IataCode  
+
+aaaa=IataCode("Melbourne")
+print(aaaa)
