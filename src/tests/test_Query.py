f9fb2d5bdc0f5e68e33bb8051d6bcf9e2cb5cd96
Sean <ubuntu@ip-172-31-44-214.ec2.internal>
2020-10-02 01:00:33 +0000

    finding the right commands for ci (2)

--- src/tests/test_Query.py
+++ src/tests/test_Query.py
@@ -1,7 +1,7 @@
 import unittest
 from mock import patch
-# import sys
-# sys.path.append('../src')
+import sys
+sys.path.append('../')
 from Query import Query
 
 class TestQueryClass(unittest.TestCase):
